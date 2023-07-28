4167d9d641e969fa2b5335fe33d094481fa26d1e
Sazid9 <shazid.faiyaz@gmail.com>
2023-07-29 02:10:36 +0600

    new

--- CSE220/recursion.py
+++ CSE220/recursion.py
@@ -1,5 +1,4 @@
 
-
 def fact(n):
     if n == 0 or n == 1:
         return n  #base function
@@ -25,3 +24,84 @@ def recursivesm(head):
 #Length of a String
 
 def strlength(str):
+    if len(str) == 0:
+        return 0
+    else:
+        return 1+strlength(str[1:])
+
+#Length of a linked list:
+
+def lenLinkedlist(head):
+    if head == None:
+        return 0
+    else:
+        return 1+lenLinkedlist(head.next)
+
+#Sequential search in a sequence:
+
+def sequnsearch(head,key):
+    if head == None:
+        return False
+    elif head.element == key:
+        return True
+    else:
+        return sequnsearch(head.next, key) #recursive
+
+def seqsearch(arrar,key):
+    if len(arrar) == 0:
+        return False
+    elif arrar[0] == key:
+        return True
+    else:
+        return seqsearch(arrar[1:],key) #recursive
+
+def contains(arr, left, key):
+    if left >= len(arr):
+        return False
+    elif arr[left] == key:
+        return True
+    else:
+        return contains(arr,left+1,key)
+
+#Binary search in a sorted array:
+
+def contain(arr, left, right, key):
+    if left > right:
+        return False #base case
+    else:
+        mid = (left+right)//2
+        if key == arr[mid]:
+            return True
+        elif key > arr[mid]:
+            return contain(arr, mid + 1, right, key)
+        else:
+            return contain(arr, left, mid-1, key)
+#Finding the maximum in a sequence
+def maximum(a,b):
+    return a if a >= b else b
+
+def findMax(head):
+    if head.next == None:
+        return head.element
+    else:
+        newmax = findMax(head.next)
+        return maximum(head.element, newmax)
+# for array
+def arrayMax(arr, left):
+    if left == len(arr)-1:
+        return arr[left]
+    else:
+        maxreset = arrayMax(arr, left+1)
+        return maximum(arr[left], maxreset)
+
+#Finding the maximum in an array
+def max(arr, left, right):
+    if left == right:
+        return arr[left]
+    else:
+        mid = (left + right)//2
+        maxlefthalf = max(arr, left, mid)
+        maxrighthalf = max(arr, mid+1, right)
+        return maximum(maxlefthalf, maxrighthalf)
+
+
