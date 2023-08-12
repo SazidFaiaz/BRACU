ea845628122b31defa3da3ad1f40e03db7f1799c
Sazid9 <shazid.faiyaz@gmail.com>
2023-08-12 16:40:44 +0600

    new

--- CSE220/lab06/task05.py
+++ CSE220/lab06/task05.py
@@ -2,19 +2,19 @@ class FinalQ:
 
     def printt(self, array, idx):
         if idx < len(array):
-            profit = self.calcProfit(array[idx])
-            print("{}. Investment: {}; Profit: {}".format(idx + 1, array[idx], self.calcProfit(array[idx])))
+            profit = self.calprofit(array[idx])
+            print(f"profit for investment: {array[idx]} and calculated profit: {profit} USD")
             self.printt(array, idx + 1)
 
-    def calcProfit(self, investmentt):
-        if investmentt == 25000:
+    def calprofit(self, investmentt):
+        if investmentt == 20000:
             return 0.0
         else:
             if investmentt <= 100000:
-                return self.calcProfit(investmentt - 1000) + 45.0
+                return self.calprofit(investmentt - 1000) + 45.0
             else:
-                return self.calcProfit(investmentt - 1000) + 80.0
+                return self.calprofit(investmentt - 1000) + 80.0
+
 
 array = [25000, 100000, 250000, 350000]
-f = FinalQ()
-f.printt(array, 0)
+FinalQ().printt(array, 0)
