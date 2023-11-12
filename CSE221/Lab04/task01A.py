928cd4cd9b83225c2a9ab314f2e2667183f15fe4
Sazid9 <shazid.faiyaz@gmail.com>
2023-11-12 19:32:51 +0600

    new

---
+++ CSE221/Lab04/task01A.py
@@ -0,0 +1,22 @@
+
+filein = open("input01A.txt", "r")
+fileout = open("output01A.txt", "w")
+
+N, M = map(int, filein.readline().split())
+print(N, M)
+
+matrix = [[0] * (N + 1) for _ in range(N + 1)]
+for i in range (M):
+    u, v, w = map(int, filein.readline().strip().split(" "))
+    print(u, v, w)
+    matrix[u][v] = w
+# print(matrix)
+
+def printmatrix(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print(matrix[i][j], end=' ')
+            fileout.write(f'{matrix[i][j]} \n ')
+        print()
+
+printmatrix(matrix)
